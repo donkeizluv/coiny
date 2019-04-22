030f905ee004061a998905a37b07432c87e3c3bf
Luu Hong <donkeizluv@gmail.com>
2019-04-23 00:22:52 +0700

    fix static assets path

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 let app = express();
 // add middlewares and stuff
 app.use(require("./middlewares/compression"));
 app.use(require("./middlewares/cors"));
-app.use(require("./middlewares/static")("/dist"));
+app.use(require("./middlewares/static")(path.join(__dirname, "/dist")));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
   bodyParser.urlencoded({
--- middlewares/static.js
+++ middlewares/static.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const path = require("path");
 
 module.exports = function(distPath) {
-  return express.static(path.join(__dirname, distPath));
+  return express.static(distPath);
 };
